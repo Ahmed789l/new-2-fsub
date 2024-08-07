518c5d0a9fbb13947a759bf9d429cf9fa703da0c
I'm Sanji <filesharingbot78@gmail.com>
2024-08-07 23:55:53 +0530

    Add files via upload

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,13 +10,13 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ 𝐃ᴇᴠᴇʟᴏᴘᴇᴅ 𝐁ʏ : <a href='https://t.me/Straw_Hat_Bots'>𝐒ᴛʀᴀᴡ 𝐇ᴀᴛ ꭙ 𝐁ᴏᴛs</a>\n○ 𝐁ᴏᴛ 𝐔ᴘᴅᴀᴛᴇs : <a href='https://t.me/Straw_Hat_Bots'>𝐂ʟɪᴄᴋ 𝐇ᴇʀᴇ</a>\n○ 𝐁ᴏᴛ 𝐒ᴜᴘᴘᴏʀᴛ : <a href='https://t.me/Straw_hat_support'>𝐂ʟɪᴄᴋ 𝐇ᴇʀᴇ</a>\n○ 𝐀ɴɪᴍᴇ 𝐂ʜᴀᴛ : <a href='https://t.me/+BSL2tOp6BGE2MGU1'>𝐒ᴛʀᴀᴡ 𝐇ᴀᴛ ꭙ 𝐆ʀᴏᴜᴘ</a></b>",
+            text = f"<b>○ 𝐎ᴡɴᴇʀ : <a href='https://t.me/WhiteBeard_sama'>𝐖𝐃𝐆 - 𝟓</a>\n○ 𝐀ɴɪᴍᴇ 𝐂ʜᴀɴɴᴇʟ : <a href='https://t.me/Shirohige_Animes'>𝐒ʜɪʀᴏʜɪɢᴇ 𝐀ɴɪᴍᴇ</a>\n○ 𝐎ɴɢᴏɪɴɢ 𝐂ʜᴀɴɴᴇʟ : <a href='https://t.me/Ongoing_Anime_WDG_5'>𝐎ɴɢᴏɪɴɢ 𝐖𝐃𝐆 - 𝟓</a>\n○ 𝐇ᴇᴍᴛᴀɪ 𝐂ʜᴀɴɴᴇʟ : <a href='https://t.me/+zPM3HDvFOvg5Yjg1'>𝐃𝐒𝐇 - 𝐁𝐀𝐂𝐊𝐔𝐏</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
                     InlineKeyboardButton("⚡️ Cʟᴏsᴇ", callback_data = "close"),
-                    InlineKeyboardButton('🍁 Dᴇᴠᴇʟᴏᴘᴇʀs', url='https://t.me/Straw_hat_bots')
+                    InlineKeyboardButton('🧑‍💻 Dᴇᴠᴇʟᴏᴘᴇʀs', url='https://t.me/Straw_hat_bots')
                     ]
                 ]
             )
